aa1adab196a35ce40e564d45df0cceee2a34d1e3
Keerthi Sravan Ravi <sravan953@gmail.com>
2018-04-21 18:53:04 -0400

    Tqdm wrapper while creating dataset

--- create_dataset.py
+++ create_dataset.py
@@ -2,6 +2,7 @@ import glob
 import librosa
 import numpy as np
 import os
+from tqdm import tqdm
 
 
 PATH_TO_DATA = os.path.join(os.getcwd(), 'data')
@@ -12,10 +13,10 @@ INVALID_LABELS = ["bed", "bird", "cat", "dog", "happy", "house", "marvin", "shei
 
 def read_audio_file(audio_file_path):
     """Read the .wav file at {audio_file_path} and return it a numpy array
-    
+
     Arguments:
         audio_file_path {str} -- absolute path to audio file
-    
+
     Returns:
         numpy.ndarray -- The audio file loaded as a numpy array
     """
@@ -38,31 +39,31 @@ def read_audio_file(audio_file_path):
 
 def get_mel_power_spectrogram(audio_file, n_fft=1024, hop_length=256, fmax=3000):
     """Generates mel power spectrogram of the {audio_file}
-    
+
     Arguments:
         audio_file {numpy.ndarray} -- The audio file
-    
+
     Keyword Arguments:
         n_fft {int} -- Length of FFT (default: {1024})
         hop_length {int} -- The hop length (default: {256})
         fmax {int} -- The maximum frequency of the mel filterbank (default: {3000})
-    
+
     Returns:
         numpy.ndarray -- Mel power spectrogram
     """
-    return librosa.power_to_db(librosa.feature.melspectrogram(audio_file, 
-                                                              sr=SAMPLING_RATE, 
-                                                              n_fft=n_fft, 
-                                                              hop_length=hop_length, 
-                                                              fmax=fmax), 
+    return librosa.power_to_db(librosa.feature.melspectrogram(audio_file,
+                                                              sr=SAMPLING_RATE,
+                                                              n_fft=n_fft,
+                                                              hop_length=hop_length,
+                                                              fmax=fmax),
                                ref=np.max)
 
 def return_valid_label(audio_label):
     """Check if label of audio file is valid
-    
+
     Arguments:
         audio_label {str} -- The label of audio file
-    
+
     Returns:
         {str} -- The valid label
     """
@@ -70,7 +71,7 @@ def return_valid_label(audio_label):
     return "unknown" if audio_label in INVALID_LABELS else audio_label
 
 def create_data_split_spectrogram():
-    """Split the data into train, validation and test and save to disk as numpy arrays 
+    """Split the data into train, validation and test and save to disk as numpy arrays
     """
 
     train_data = []
@@ -79,19 +80,19 @@ def create_data_split_spectrogram():
     validation_labels = []
     test_data = []
     test_labels = []
-    
+
     print('Creating train data')
-    for path in train_data_paths:
+    for path in tqdm(train_data_paths):
         train_data.append(get_mel_power_spectrogram(read_audio_file(path)))
         train_labels.append(return_valid_label(path.split(os.path.sep)[0]))
-        
+
     print('Creating validation data')
-    for path in validation_data_paths:
+    for path in tqdm(validation_data_paths):
         validation_data.append(get_mel_power_spectrogram(read_audio_file(path)))
         validation_labels.append(return_valid_label(path.split(os.path.sep)[0]))
-    
+
     print('Creating test data')
-    for path in test_data_paths:
+    for path in tqdm(test_data_paths):
         test_data.append(get_mel_power_spectrogram(read_audio_file(path)))
         test_labels.append(return_valid_label(path.split(os.path.sep)[0]))
 
@@ -99,7 +100,7 @@ def create_data_split_spectrogram():
     train_data = (np.array(train_data) - np.mean(train_data)) / np.std(train_data)
     validation_data = (np.array(validation_data) - np.mean(validation_data)) / np.std(validation_data)
     test_data = (np.array(test_data) - np.mean(test_data)) / np.std(test_data)
-    
+
     print('Saving the data in ' + PATH_TO_DATA)
     np.save(os.path.join(PATH_TO_DATA, 'train_data'), train_data)
     np.save(os.path.join(PATH_TO_DATA, 'train_labels'), train_labels)
